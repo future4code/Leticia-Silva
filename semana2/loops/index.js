16133153afde31861d98526806a942f7bf73dc99
Leticia Martins Silva <wakawashi2030@gmail.com>
2021-08-18 16:59:20 -0300

    Criada pasta semana2-aula09 e loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,86 @@
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+// resposta
+//O que o código abaixo está fazendo? Qual o resultado impresso no console?
+//a - simplificando a forma de escrever os laços, condição de continuação atrelado
+//a um numero que é incrementado; 
+//
+//enqunto i for menor que 5 e incrementa mais 1
+//ação repetida suscinta e verificação de uma vez só
+// 
+//enquanto a condição for atendida
+// as linhas de código dentro do bloco
+//serão executadas, quando a condição
+//não atender o loop vai parar
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+//a) O que vai ser impresso no console?19,21,23,25,27,30
+//b) Se eu quisesse acessar o índice de cada 
+//elemento dessa lista, o for...of... é suficiente? Se sim, o que poderia ser usado 
+//para fazer isso?
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+
+//Escrita de código
+
+//Pergunte ao usuário quantos bichinhos de estimação ele tem e
+// guarde esse dado em uma variável. 
+
+//a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
+//b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles,
+// um por um, e guarde esses nomes em um array
+//Coloque o seu prompt dentro de um loop. Esse loop deve ser executado a mesma quantidade
+// de vezes que o usuário inseriu. Por exemplo: se o usuário tem 4 pets, ele deve conseguir inserir 4 nomes.
+
+const quantosBichinhosEstimacaoTem = Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+    if(quantosBichinhosEstimacaoTem===0)
+    
+    console.log("Que pena!Você pode adotar um pet!")
+
+  
+
+const nomePet = prompt("digite pet)array.push(nomePet)
+
+    quantosBichinhosEstimacaoTem++
+
+    }
+
+    
+
+
+
+
+
+  
+
+//const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
