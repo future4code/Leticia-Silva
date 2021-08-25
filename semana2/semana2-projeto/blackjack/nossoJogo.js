776a5f6cfa3a0aa9a023c21267c832b629deecd4
Leticia Martins Silva <wakawashi2030@gmail.com>
2021-08-25 16:50:19 -0300

    implementando css na loja

--- semana2/semana2-projeto/blackjack/nossoJogo.js
+++ semana2/semana2-projeto/blackjack/nossoJogo.js
@@ -3,7 +3,7 @@
  * 
  * 
     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
+   
     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
  * 
@@ -12,8 +12,8 @@
  */
    //1 - Imprime uma mensagem no console "Boas vindas ao jogo de Blackjack!".
 
-    console.log ("Boas vindas ao jogo de Blackjack!")
-    
+      console.log ("Boas vindas ao jogo de Blackjack!")
+
    //  confirm é um comando que cria uma caixa com dois botões: ok e cancelar, permitindo
    //  fazer perguntas de "sim" e "não". Caso o usuário responda sim (ok) o comando devolve 
    //o valor true, caso contrário, devolve false. Desta forma, podemos usar condicionais 
@@ -22,12 +22,12 @@
   //2 - Envia um confirm, perguntando ao usuário: "Quer iniciar uma nova rodada?".
   //3 - Se o usuário responder `cancel`, imprime uma mensagem no console "O jogo acabou".
   //4 - Se o usuário responder `Ok`, o programa deve iniciar uma nova rodada.
-    
-  if(confirm("Quer iniciar uma nova rodada?")) {
+
+   if(confirm("Quer iniciar uma nova rodada?")) {
 	// o que fazer se o usuário clicar "ok"
    cartaUsuario = comprarCarta()
    cartaComputador = comprarCarta()
-    
+   
    console.log(cartaUsuario.texto) 
    console.log(cartaUsuario.valor) 
    console.log(cartaComputador.texto)
@@ -41,7 +41,7 @@
 
 
 
-  
+
    // console.log(cartaComputador) = `Computador - cartas: ${cartaComputador.texto} 
    // - pontuação ${cartaComputador.valor}`
 
